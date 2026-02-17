e4539e272701171c9627ba7592bd3e7537023acc
Fangtangtang <2167295472@qq.com>
2026-02-17 16:54:28 -0500

    [feat] affine expression

--- mlir/extras/dialects/affine.py
+++ mlir/extras/dialects/affine.py
@@ -1,2 +1,39 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+
+from ...dialects import affine
+from ...ir import AffineConstantExpr, AffineExpr
+
+
+class AffExpr:
+    @staticmethod
+    def constant(value):
+        return AffineConstantExpr.get(value)
+
+    @staticmethod
+    def symbol(idx: int):
+        return AffineExpr.get_symbol(idx)
+
+    @staticmethod
+    def dim(idx: int):
+        return AffineExpr.get_dim(idx)
+
+    @staticmethod
+    def add(lhs, rhs):
+        return lhs + rhs
+
+    @staticmethod
+    def sub(lhs, rhs):
+        return lhs - rhs
+
+    @staticmethod
+    def mul(lhs, rhs):
+        return lhs * rhs
+
+    @staticmethod
+    def div(lhs, rhs):
+        return AffineExpr.get_floor_div(lhs, rhs)
+
+    @staticmethod
+    def mod(lhs, rhs):
+        return lhs % rhs
