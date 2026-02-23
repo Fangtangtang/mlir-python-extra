908c1193c8c828c77ce2d5a103ca480a9b4d9b8a
Fangtangtang <2167295472@qq.com>
2026-02-23 15:51:33 -0500

    [feat] parse affine expression to python expression

--- mlir/extras/dialects/affine.py
+++ mlir/extras/dialects/affine.py
@@ -1,8 +1,18 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import sympy
 from ...dialects import affine
-from ...ir import AffineConstantExpr, AffineExpr
+from ...ir import (
+    AffineExpr,
+    AffineConstantExpr,
+    AffineAddExpr,
+    AffineMulExpr,
+    AffineModExpr,
+    AffineFloorDivExpr,
+    AffineSymbolExpr,
+    AffineDimExpr,
+)
 
 
 class AffExpr:
@@ -12,11 +22,11 @@ class AffExpr:
 
     @staticmethod
     def symbol(idx: int):
-        return AffineExpr.get_symbol(idx)
+        return AffineSymbolExpr.get(idx)
 
     @staticmethod
     def dim(idx: int):
-        return AffineExpr.get_dim(idx)
+        return AffineDimExpr.get(idx)
 
     @staticmethod
     def add(lhs, rhs):
@@ -32,8 +42,48 @@ class AffExpr:
 
     @staticmethod
     def div(lhs, rhs):
-        return AffineExpr.get_floor_div(lhs, rhs)
+        return AffineFloorDivExpr.get(lhs, rhs)
 
     @staticmethod
     def mod(lhs, rhs):
         return lhs % rhs
+
+    @staticmethod
+    def dim_name(d: AffineDimExpr) -> str:
+        return f"d{d.position}"
+
+    @staticmethod
+    def symbol_name(d: AffineDimExpr) -> str:
+        return f"s{d.position}"
+
+    @staticmethod
+    def parse(expr):
+        def try_cast(cls, expr_):
+            try:
+                return cls(expr_)
+            except ValueError:
+                return None
+
+        if (c := try_cast(AffineConstantExpr, expr)) is not None:
+            return sympy.Integer(c.value)
+        if (d := try_cast(AffineDimExpr, expr)) is not None:
+            return sympy.Symbol(AffExpr.dim_name(d))
+        if (s := try_cast(AffineSymbolExpr, expr)) is not None:
+            return sympy.Symbol(AffExpr.symbol_name(s))
+        if (a := try_cast(AffineAddExpr, expr)) is not None:
+            lhs = AffExpr.parse(a.lhs)
+            rhs = AffExpr.parse(a.rhs)
+            return lhs + rhs
+        if (m := try_cast(AffineMulExpr, expr)) is not None:
+            lhs = AffExpr.parse(m.lhs)
+            rhs = AffExpr.parse(m.rhs)
+            return lhs * rhs
+        if (mod := try_cast(AffineModExpr, expr)) is not None:
+            lhs = AffExpr.parse(mod.lhs)
+            rhs = AffExpr.parse(mod.rhs)
+            return lhs % rhs
+        if (fd := try_cast(AffineFloorDivExpr, expr)) is not None:
+            lhs = AffExpr.parse(fd.lhs)
+            rhs = AffExpr.parse(fd.rhs)
+            return lhs // rhs
+        raise TypeError(f"Unsupported AffineExpr type: {type(expr)}")
