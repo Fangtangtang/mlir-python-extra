574498fe283c954c58719caf1bbde2e8c9db7f18
Fangtangtang <2167295472@qq.com>
2026-02-24 20:18:49 -0500

    update

--- mlir/extras/dialects/func.py
+++ mlir/extras/dialects/func.py
@@ -2,6 +2,17 @@ from ...dialects import func
 from ...ir import StringAttr, FunctionType
 
 
+def set_string_attr_if_present(op, name, value):
+    if value is None:
+        return
+    if isinstance(value, list):
+        value = "".join(value)
+    if isinstance(value, str):
+        op.attributes[name] = StringAttr.get(value)
+    else:
+        raise TypeError(f"{name} must be str or list[str], got {type(value)}")
+
+
 def function(
     func_name,
     itypes,
@@ -17,8 +28,6 @@ def function(
         func_op.attributes["sym_visibility"] = StringAttr.get("private")
     else:
         func_op.add_entry_block()
-    if itype_hints is not None:
-        func_op.attributes["itypes"] = StringAttr.get("".join(itype_hints))
-    if otype_hints is not None:
-        func_op.attributes["otypes"] = StringAttr.get("".join(otype_hints))
+    set_string_attr_if_present(func_op, "itypes", itype_hints)
+    set_string_attr_if_present(func_op, "otypes", otype_hints)
     return func_op
