def5dfc72c2ee740f39c46663b7c54fead302f69
Fangtangtang <2167295472@qq.com>
2026-03-02 15:40:58 -0500

    [feat] transform

---
+++ mlir/extras/dialects/transform.py
@@ -0,0 +1,16 @@
+# Copyright Allo authors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from ...dialects import transform as xform_d
+from ...dialects.builtin import ModuleOp
+from ...ir import InsertionPoint, UnitAttr
+
+
+def module(name: str = None):
+    if name is None:
+        name = "__transform_main"  # default interpreter entry point
+    mod = ModuleOp()
+    mod.attributes["transform.with_named_sequence"] = UnitAttr.get()
+    with InsertionPoint(mod.body):
+        xform_d.named_sequence(name, [xform_d.any_op_t()])
+    return mod
